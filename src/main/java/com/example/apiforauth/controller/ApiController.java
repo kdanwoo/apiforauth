9ffdf33e0b76df393327190c7204ba6ec8a3b7d6
danwook <k.danwoo91@ibksystem.co.kr>
2021-06-30 11:13:04 +0900

    Initial commit

---
+++ src/main/java/com/example/apiforauth/controller/ApiController.java
@@ -0,0 +1,10 @@
+package com.example.apiforauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+}
---
+++ src/test/java/com/example/apiforauth/ApiforauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiforauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiforauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
