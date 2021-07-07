f1a435d849cbfc70fb6a303ce923557142ec9661
danwook <k.danwoo91@ibksystem.co.kr>
2021-07-07 17:57:31 +0900

    API Resource 서버 설정

--- src/main/java/com/example/apiforauth/config/Oauth2ResourceServerConfig.java
+++ src/main/java/com/example/apiforauth/config/Oauth2ResourceServerConfig.java
@@ -1,10 +1,23 @@
 package com.example.apiforauth.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+@Slf4j
 @Configuration
 @EnableResourceServer
 public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
@@ -16,4 +29,30 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
                 .antMatchers("/v1/books").access("#oauth2.hasScope('read')")
                 .anyRequest().authenticated();
     }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        Resource resource = new ClassPathResource("publicKey.txt");
+        String publicKey = null;
+        try {
+            publicKey = asString(resource);
+            log.info("Jwt Verifier Key : {} ", publicKey);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
+        return converter;
+    }
+
+    public static String asString(Resource resource) throws IOException {
+        Reader reader = new InputStreamReader(resource.getInputStream(), "UTF-8");
+        return FileCopyUtils.copyToString(reader);
+    }
+
 }
--- src/main/java/com/example/apiforauth/controller/ApiController.java
+++ src/main/java/com/example/apiforauth/controller/ApiController.java
@@ -1,15 +1,13 @@
 package com.example.apiforauth.controller;
 
+import com.example.apiforauth.config.CommonConfig;
 import com.example.apiforauth.entity.Book;
 import com.example.apiforauth.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RequestMapping("/v1")
@@ -17,9 +15,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ApiController{
 
+    private final CommonConfig commonConfig;
     private final BookService bookService;
 
-    @GetMapping("/books")
+    @PostMapping("/books")
     public List<Book> all(){
         return bookService.findAll();
     }
